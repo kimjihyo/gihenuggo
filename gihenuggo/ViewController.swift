e319437a4c8d0772134ce70b156f7b85bb4db109
김지효 <kimjihyo@gimjihyoui-macbook-pro.local>
2016-05-01 18:55:17 +0900

    Initial Commit

---
+++ gihenuggo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gihenuggo
+//
+//  Created by 김지효 on 2016. 5. 1..
+//  Copyright © 2016년 김지효. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
